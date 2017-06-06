4e96d99eaf3d40b3f8d8d044a856fa84cba9cbcf
Michal Kizling <mkizling@gmail.com>
2017-06-06 11:31:17 +0200

    Base project files

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var a = prompt('Enter length of triangle base');
+var h = prompt('Enter height of triangle');
+var triangleArea = 0;
+
+triangleArea = a * h / 2;
+
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to '+ triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to '+ triangleArea);
